6b3b1b34776b6ce03e0249eaa21df87adfd99ce8
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-06-28 21:31:20 +0200

    Messing around with grid layouts

---
+++ intro/main4.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+label_1 = tk.Label(root, text='Name')
+label_2 = tk.Label(root, text='Password')
+entry_1 = tk.Entry(root)
+entry_2 = tk.Entry(root)
+
+label_1.grid(row=0, sticky=tk.E)
+label_2.grid(row=1, sticky=tk.E)
+entry_1.grid(row=0,column=1)
+entry_2.grid(row=1,column=1)
+
+c = tk.Checkbutton(root, text='Keep me logged in')
+c.grid(columnspan=2)
+
+root.mainloop()
