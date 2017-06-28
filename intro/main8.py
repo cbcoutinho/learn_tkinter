0a9fa8eb78d915957187650242ab433eb0ad6bff
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-06-28 22:59:49 +0200

    Add a toolbar

--- intro/main8.py
+++ intro/main8.py
@@ -11,6 +11,8 @@ def doNothing():
 
 root = tk.Tk()
 
+# **** Main Menu ****
+
 menu = tk.Menu(root)
 root.config(menu=menu)
 
@@ -24,5 +26,15 @@ subMenu.add_command(label='Exit', command=root.quit)
 editMenu = tk.Menu(menu)
 menu.add_cascade(label='Edit', menu=editMenu)
 editMenu.add_command(label='Redo', command=doNothing)
+
+# **** Toolbar ****
+
+tb = tk.Frame(root, bg='blue')
+button1 = tk.Button(tb, text='Insert Image', command=doNothing)
+button1.pack(side=tk.LEFT, padx=2, pady=2)
+button2 = tk.Button(tb, text='Print', command=doNothing)
+button2.pack(side=tk.LEFT, padx=2, pady=2)
+
+tb.pack(side=tk.TOP, fill=tk.X)
 
 root.mainloop()
