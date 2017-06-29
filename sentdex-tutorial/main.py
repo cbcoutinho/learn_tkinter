3a8fefc1b01b9a1f536d332fe2adafe106a09166
Chris Coutinho <chrisbcoutinho@gmail.com>
2017-06-29 16:37:35 +0200

    Add a status bar and a button The button uses a lambda to call a function on click

--- sentdex-tutorial/main.py
+++ sentdex-tutorial/main.py
@@ -10,6 +10,7 @@ class SeaofBTCapp(tk.Tk):
     def __init__(self, *args, **kwargs):
 
         tk.Tk.__init__(self, *args, **kwargs)
+        self.title("The title")
         container = tk.Frame(self)
 
         container.pack(side='top',
@@ -37,12 +38,29 @@ class SeaofBTCapp(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
 
+def quickfun(string):
+    print(string)
+
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text='Start Page', font=LARGE_FONT)
+        label = tk.Label(self,
+                         text='Start Page',
+                         font=LARGE_FONT)
         label.pack(pady=10,padx=10)
+
+        button1 = tk.Button(self,
+                            text='Visit Page 1',
+                            command=lambda: quickfun('This worked'))
+        button1.pack()
+
+        status = tk.Label(self,
+                          text='Status bar...',
+                          bd=1,
+                          relief=tk.SUNKEN,
+                          anchor=tk.W)
+        status.pack(side=tk.BOTTOM, fill=tk.X)
 
 app = SeaofBTCapp()
 app.mainloop()
